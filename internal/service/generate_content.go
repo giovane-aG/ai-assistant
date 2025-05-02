7cf037064f372311cd9062992b03a522a01f3d69
Giovane Almeida <giovane.almeida@ifood.com.br>
2025-05-02 10:15:15 -0300

    Add GenerateContentService for content generation
    - Introduced `GenerateContentService` to handle content generation requests.
    - Defined `IGenerateContentClient` interface for generating content with a specified model and content string.
    - Implemented a constructor for `GenerateContentService` to initialize it with a content client.

---
+++ internal/service/generate_content.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"context"
+
+	"github.com/giovane-aG/ai-assistant/internal/model"
+)
+
+type IGenerateContentClient interface {
+	GenerateContent(
+		ctx context.Context,
+		model string,
+		content string,
+		// config *GenerateContentConfig,
+	) (*model.GenerateContentResponse, error)
+}
+
+type GenerateContentService struct {
+	generateContentClient IGenerateContentClient
+}
+
+func NewGenerateContentService(generateContentClient IGenerateContentClient) *GenerateContentService {
+	return &GenerateContentService{
+		generateContentClient: generateContentClient,
+	}
+}
+
+func (s *GenerateContentService) Generate(ctx context.Context, model string, content string) (*model.GenerateContentResponse, error) {
+	return s.generateContentClient.GenerateContent(ctx, model, content)
+}
